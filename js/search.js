ebd581863becaccdc46339e5359772d8753fc3d8
nhatdang1114 <75656468+nhatdang1114@users.noreply.github.com>
2020-12-08 11:37:29 +0700

    Update search.js

--- js/search.js
+++ js/search.js
@@ -25,7 +25,7 @@
       var pair = vars[i].split('=');
 
       if (pair[0] === variable) {
-        return decodeURIComponent(pair[1]);
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
       }
     }
   }
